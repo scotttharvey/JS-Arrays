ce7d4bb78a73db56fb34e7116282b493a10534e1
Scott Harvey <scottyharvey33@gmil.com>
2016-05-16 17:03:38 -0600

    Just About done

--- arraysPractice.js
+++ arraysPractice.js
@@ -4,7 +4,9 @@
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
-  //Code Here
+  function first(arr){
+    return arr[0];
+  }
 
 
 //Next problem
@@ -15,7 +17,9 @@ var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
 
 
-  //Code Here
+  function last(arr){
+    return arr[arr.length - 1];
+  }
 
 
 //Next Problem
@@ -24,7 +28,11 @@ var arr = [40,50,60];
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
+function looper(family){
+  for(var i = 0; i < family.length; i++){
+  alert(family[i]);
+  }
+}
 
 
 //Next problem
@@ -34,7 +42,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
-  //Code Here
+function reversedLooper(letters){
+  for (var i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+
+  }
+}
 
 
 //Next Problem
@@ -43,9 +56,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
-  //Code Here
-
-
+function evenFinder(nums){
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 !== 0){
+    nums.splice(i, 1);
+    }
+  }
+return nums;
+}
 //Next problem
 
 
@@ -55,7 +73,21 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 
 
-  //Code Here
+function divider(numbersArray){
+  var evens = [];
+  var odds = [];
+  var oneMore = [];
+  for (var i = 0; i < numbersArray.length; i++) {
+    if(numbersArray[i] % 2 !== 0){
+      odds.push(numbersArray[i]);
+    }
+      else {
+        evens.push(numbersArray[i]);
+      }
+
+  }
+  return oneMore = [evens, odds];
+}
 
 
 //Next Problem
@@ -69,7 +101,17 @@ var getRandomArbitrary = function() {
 
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
+function finder(arr){
+  var randomNum= getRandomArbitrary();
+  // var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(randomNum === arr[i]){
+      return true;
+    }
+
+  }
+  return false;
+}
 
   //Code Here
 
@@ -81,8 +123,11 @@ var getRandomArbitrary = function() {
 var str = 'this is my sentence';
 //Write a function called reverse that takes a given str as it's only argument and returns that string after it's been reversed
 
-  //Code Here
+function reverse(str){
+  var strArr = str.split("").reverse().join("");
+    return strArr;
 
+}
 
 //Next Problem
 
@@ -103,7 +148,28 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   In both the removeItem function and the addItem function, you will also need to check for valid aurguments. Specrunner will try to call your functions without passing in valid aurguments. When this happens, you will need to respond by returning an empty array.
 */
 
-  //Code Here
+function removeItem(myGroceryList, rvItem){
+  for (var i = 0; i < myGroceryList.length; i++) {
+    if (arguments.length === 0){
+      return [];
+    }
+    else if (myGroceryList[i] === rvItem) {
+      myGroceryList.splice(i, 1)
+    }
+  }
+  return myGroceryList;
+}
+
+function addItem(myGroceryList, add) {
+  if (myGroceryList.indexOf(add) === -1) {
+    myGroceryList.push(add);
+    return myGroceryList;
+    if (arguments.length === 0){
+      return [];
+    }
+  }
+  return myGroceryList;
+}
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -116,7 +182,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
-  //Code Here
+function maker() {
+  var arr1 = [];
+  for (var i = 1; i <= 215; i++) {
+    arr1.push(i);
+  }
+  return arr1;
+}
 
 
 
@@ -126,8 +198,12 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 var numbers = [5, '9', 16, 19, '25', '34', 48];
 //Write a function called addTen that is given 'numbers' as it's only argument and returns a new
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
-
-  //Code Here
+function addTen(numbers){
+  for (var i = 0; i < numbers.length; i++) {
+    numbers[i] = parseInt(numbers[i]) + 10;
+  }
+  return numbers;
+}
 
 
 
@@ -148,7 +224,12 @@ for(var i = 0; i < num2; i++){
 //Above is some code that adds a random number of values to both arr1 and arr2.
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
-  //Code Here
+function longer(arr1, arr2){
+  if (arr1.length > arr2.length) {
+    return arr1;
+  }
+  return arr2;
+}
 
 
 /*As a continuation of the previous problem, write another function called 'both'.
@@ -157,7 +238,17 @@ for(var i = 0; i < num2; i++){
   Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 */
 
-  //Code Here
+function both(arr1, arr2){
+  var newArr = [];
+  for (var i = 0; i < arr1.length; i++) {
+    for (var y = 0; y < arr2.length; y++) {
+      if (arr1[i] === arr2[y]) {
+      newArr.push(i);
+    }
+    }
+  }
+    return arr1;
+}
 
 
 
